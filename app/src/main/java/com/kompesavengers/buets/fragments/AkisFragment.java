8f1d4038595d56ca5318ced6ead13429dd05f991
triforce930 <mahmutkaraca95@gmail.com>
2015-04-11 22:07:34 +0300

    fragments created

---
+++ app/src/main/java/com/kompesavengers/buets/fragments/AkisFragment.java
@@ -0,0 +1,66 @@
+package com.kompesavengers.buets.fragments;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kompesavengers.buets.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link AkisFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AkisFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment AkisFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static AkisFragment newInstance(String param1, String param2) {
+        AkisFragment fragment = new AkisFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public AkisFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_akis, container, false);
+    }
+
+
+}
